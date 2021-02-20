b1595ac759562cbe6b6a7cc0fea2cf8d27995fe3
Leandro Parrado <lparrado321@gmail.com>
2021-02-20 01:33:03 -0500

    Added web manifest to public

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,8 +10,12 @@ const FILES_TO_CACHE = [
 
 // Caches variables
 const CACHE_NAME = "site-cache-v1";
+
+//Stores all the data in here
 const DATA_CACHE_NAME = "data-cache-v1";
 
+
+//Installs data to cache
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches
@@ -19,3 +23,13 @@ self.addEventListener('install', (e) => {
             .then((cache) => cache.addALL(FILES_TO_CACHE))
     );
 });
+
+//requests all the api units
+self.addEventListener('fetch', (e) => {
+    if (e.request.url.includes("/api/")) {
+        e.respondWith(
+            //it needs to open the data_cache_name  to get the data
+            caches.open(DATA_CACHE_NAME).then((cachedResponse))
+        )
+    }
+})
